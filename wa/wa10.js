0b6decdc7b3753f23854d30e1078ec10bbab226a
Max-Pasiennik <mapa4950@colorado.edu>
2024-03-25 01:15:29 -0400

    change

--- wa/wa10.js
+++ wa/wa10.js
@@ -2,10 +2,10 @@ const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
 
-const storyText = "It was 94 fahrenheit outside, so :insertX: went for a walk. When they got to :insertY:, they stared in horror for a few moments, then :insertZ:. Bob saw the whole thing, but was not surprised — :insertX: weighs 300 pounds, and it was a hot day."
-const insertX = ["Looney tooney", "Flo Willy", "Tankity Tank"];
-const insertY = ["Puerto Vallerta", "the dog hotel", "Grandview"];
-const insertZ = ["rolled in the dirt", "doordashed Birdcall", "made a TikTok with Charlie Demilio"];
+const storyText = "It was 100 fahrenheit outside, so :insertX: went for a walk. When they got to :insertY:, they stared in disbelief for a few moments, then :insertZ:. Gregory saw the whole thing, but was not surprised — :insertX: weighs 600 pounds, and it was a hot day."
+const insertX = ["Slim Jim", "Frank the Tank", "Jimmothy James"];
+const insertY = ["Fort Collins", "the park", "Crazy Town"];
+const insertZ = ["jumpec in the mud", "ordered wings", "made an IG short"];
 
 function randomValueFromArray(array){
   const random = Math.floor(Math.random()*array.length);
@@ -37,7 +37,7 @@ newStory = newStory.replace(":insertX:", xItem);
     let weight = Math.round(300/14);
     weight = weight + "stone";
     let temperature =  Math.round(94-32)*(5/9);
-    
+
     temperature = temperature + "centigrade";
     newStory = newStory.replace("300 pounds", weight);
     newStory = newStory.replace("94 fahrenheit", temperature);
