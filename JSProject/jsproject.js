0786de41751eb356e7f320acb95756571c577597
Max-Pasiennik <mapa4950@colorado.edu>
2024-04-30 09:37:07 -0600

    change

--- JSProject/jsproject.js
+++ JSProject/jsproject.js
@@ -1,37 +1,38 @@
-const prevEl = document.querySelector(".dec")
-const nextEl = document.querySelector(".inc")
+const prevEl = document.querySelector(".dec");
+const nextEl = document.querySelector(".inc");
 const resetEl = document.querySelector(".reset");
-const buttons = document.querySelector(".btn")
-let countEl = document.querySelector(".count");
+const countEl = document.querySelector(".count");
 
 let counts = 0;
 
-prevEl.addEventListener("click",decrease);
-nextEl.addEventListener("click",increase);
-resetEl.addEventListener("click",reset);
+prevEl.addEventListener("click", decrease);
+nextEl.addEventListener("click", increase);
+resetEl.addEventListener("click", reset);
 
-function increase(){
-    counts++;
-    countEl.innerHTML = counts;
-    if(counts>0){
+function increase() {
+    counts += 0.1;
+    countEl.innerHTML = counts.toFixed(1); // Limit the decimal to 1 place
+    if (counts > 0) {
         countEl.style.color = "green";
     }
-    if(counts==0){
+    if (counts == 0) {
         countEl.style.color = "black";
     }
 }
-function decrease(){
-    counts--;
-    countEl.innerHTML = counts;
-    if(counts<0){
+
+function decrease() {
+    counts -= 0.1;
+    countEl.innerHTML = counts.toFixed(1); // Limit the decimal to 1 place
+    if (counts < 0) {
         countEl.style.color = "red";
     }
-    if(counts==0){
+    if (counts == 0) {
         countEl.style.color = "black";
     }
 }
-function reset(){
-    counts=0;
-    countEl.innerHTML = counts;
+
+function reset() {
+    counts = 0;
+    countEl.innerHTML = counts.toFixed(1); // Limit the decimal to 1 place
     countEl.style.color = "black";
 }
