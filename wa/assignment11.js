43fd9331e0ff0fc51667a0f24d69171f1518c5b5
Max-Pasiennik <mapa4950@colorado.edu>
2024-05-05 16:00:45 -0600

    change

--- wa/assignment11.js
+++ wa/assignment11.js
@@ -1,39 +1,45 @@
-const btn = document.querySelector('#js-new-quote');
-const factCountInput = document.querySelector('#js-fact-count');
+const newQuoteButton = document.querySelector("#js-new-quote");
+newQuoteButton.addEventListener("click", async (event) => {
+  if (event && event.type === "click") {
+    console.log("Button clicked");
+    await getQuote();
+  }
+});
 
-btn.addEventListener('click', getQuote);
-
-const baseEndpoint = 'https://uselessfacts.jsph.pl/';
+const googleButton = document.querySelector("#js-google");
+googleButton.addEventListener("click", googleQuote);
 
 async function getQuote() {
-  const factCount = parseInt(factCountInput.value, 10);
-  let endpoint = baseEndpoint;
-  
-  if(factCount < 1 || factCount > 3) {
-    alert(`Error: Please enter a number between 1 and 3. You entered: ${factCount}`);
-    return;
-  }
-  
-  if(factCount > 1) {
-    endpoint += `?count=${factCount}`;
-  }
+  const apiUrl = "https://api.quotable.io/random";
 
   try {
-    const response = await fetch(endpoint);
+    const response = await fetch(apiUrl);
     if (!response.ok) {
-      throw Error(response.statusText);
+      throw new Error("Failed to fetch quote");
     }
-    const json = await response.json();
-    displayQuote(json.data.join('<br><br>')); 
-  } catch (err) {
-    console.error(err);
-    alert('Failed to fetch new quote');
+    const data = await response.json();
+
+    if (!data.content || !data.author) {
+      throw new Error("Failed to fetch quote");
+    }
+
+    console.log(`${data.content} — ${data.author}`);
+    displayQuote(`${data.content} — ${data.author}`);
+  } catch (error) {
+    console.error("Fetch error: ", error);
   }
+}
+
+async function googleQuote() {
+  const quoteTextElement = document.getElementById("js-quote-text");
+  const quote = quoteTextElement.textContent;
+  const googleUrl = `https://www.google.com/search?q=${encodeURIComponent(quote)}`;
+  window.open(googleUrl, "_blank");
 }
 
 function displayQuote(quote) {
-  const quoteText = document.querySelector('#js-quote-text');
-  quoteText.innerHTML = quote; 
+  const quoteTextElement = document.getElementById("js-quote-text");
+  quoteTextElement.textContent = quote;
 }
 
-getQuote();
+document.addEventListener("DOMContentLoaded", getQuote);
